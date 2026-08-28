0bae293edf311ca432e42daae91fde6f99763ee7
Anthony Aylward <anthony.aylward@protonmail.com>
2026-08-27 19:55:27 -0600

    getting through maturin errors

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,11 +1,11 @@
 use pyo3::prelude::*;
-use pyo3::exceptions::PyTypeError;
+// use pyo3::exceptions::PyTypeError;
 
 pub mod analysis;
 pub mod gff;
 pub mod results;
 
 #[pymodule]
-fn helixer_post_bin(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
-    Ok(())
+mod helixer_post_bin {
+    use pyo3::prelude::*;
 }
